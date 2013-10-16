d8c3e580127aff215df007ce807449eabf113f86
DiegoWin <kalinin.wincent@gmail.com>
2013-10-16 12:57:20 +0700

    Version 1.0

--- js.js
+++ js.js
@@ -1,48 +1,7 @@
-$(document).ready(function(){
-    $('.level2')
-        .click(function(event){
-            event.stopPropagation();
-        })
-        .filter(':not(:first)')
-        .hide();
-
-    $('.level').click(function(){
-        var selfClick = $(this).find('.level2:first').is(':visible');
-        if(!selfClick) {
-            $(this)
-                .parent()
-                .find('> .level2:visible')
-                .slideToggle();
-        }
-
-        $(this)
-            .find('ul:first')
-            .stop(true, true)
-            .slideToggle();
-    });
-});
-
-
-$(document).ready(function(){
-    $('#celebs ul > li ul')
-        .click(function(event){
-            event.stopPropagation();
-        })
-        .filter(':not(:first)')
-        .hide();
-
-    $('#celebs ul > li').click(function(){
-        var selfClick = $(this).find('ul:first').is(':visible');
-        if(!selfClick) {
-            $(this)
-                .parent()
-                .find('> li ul:visible')
-                .slideToggle();
-        }
-
-        $(this)
-            .find('ul:first')
-            .stop(true, true)
-            .slideToggle();
+$(function () {
+    $('.level2').not('.active').hide(); // hide all but the active
+    $('.level').click(function() {
+        $('.level2').not($(this).next('.level2')).removeClass('active').slideUp(500); // close level2
+        $(this).next('.level2').addClass('active').slideDown(500); // open level2
     });
 });
