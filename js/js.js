4f167f9347767d2a992cdee39dbfb8cbf92bbc13
DiegoWin <kalinin.wincent@gmail.com>
2013-11-14 14:49:15 +0700

    Version 1.0 semantics in code

--- js/js.js
+++ js/js.js
@@ -1,7 +1,7 @@
 $(function () {
     $('.level2').not('.active').hide(); // hide all but the active
     $('.level').click(function() {
-        $('.level2').not($(this).next('.level2')).removeClass('active').slideUp(500); // close level2
-        $(this).next('.level2').addClass('active').slideDown(500); // open level2
+        $('.level2').not($(this).find('.level2')).removeClass('active').slideUp(500); // close level2
+        $(this).find('.level2').addClass('active').slideDown(500); // open level2
     });
 });
